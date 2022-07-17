8b554746478a527c727e8c0510a59a115cd31a48
amtisyAts <2835908979@qq.com>
2022-07-17 23:12:09 +0800

    failure

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # why
 
 ### 尝试模仿知乎
+
+### 尝试失败，尝试根据别人模仿的代码模仿
+
+### 别人的作品见 https://github.com/xujiyou/zhihu-flutter
--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const WhyApp());
+  runApp(MyApp());
 }
 
-class WhyApp extends StatelessWidget {
-  const WhyApp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    Widget appBar = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Text("想法", style: TextStyle(fontSize: 16)),
-        Text("推荐", style: TextStyle(fontSize: 16)),
-        Text("热榜",style: TextStyle(fontSize: 16)),
-        Icon(Icons.punch_clock_sharp)
-      ],
-    );
-
-
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Title of Application',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        body: ListView(
-          children: [appBar],
-        ),
-      )
+      home: MyHomePage(),
     );
   }
-
-
 }
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key ? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
+
