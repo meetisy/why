74caab1c6c3a8aa165671888e852853d37b28a1a
amtisyAts <2835908979@qq.com>
2022-07-18 09:19:40 +0800

    little deletation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import '../index/index.dart';
+import '../home/home_page.dart';
+import '../global_config.dart';
 void main() => runApp(new Zhihu());
 
 class Zhihu extends StatelessWidget {
@@ -8,7 +9,8 @@ class Zhihu extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "模仿知乎",
-      home: new Index(),
+      home: new HomePage(),
+      theme: GlobalConfig.themeData,
     );
   }
 }
